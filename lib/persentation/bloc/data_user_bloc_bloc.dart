eda15fdc6c7a1ec40dbc35427b4620491b658ae9
Breakfast34 <82320414+Breakfast34@users.noreply.github.com>
2023-11-04 17:38:21 +0700

    main test commit

---
+++ lib/persentation/bloc/data_user_bloc_bloc.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:bloc/bloc.dart';
+import 'package:data_flutter_for_test/data_flutter_for_test.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'data_user_bloc_event.dart';
+part 'data_user_bloc_state.dart';
+
+class DataUserBloc extends Bloc<DataUserBlocEvent, DataUserBlocState> {
+  DataUserBloc() : super(DataUserBlocInitial()) {
+    on<DataUserBlocEvent>((event, emit) async {
+      switch (event.runtimeType) {
+        case FetchDataUserEvent:
+          await _handleDataUserEvent(event as FetchDataUserEvent, emit);
+          break;
+        default:
+      }
+    });
+  }
+
+  Future<void> _handleDataUserEvent(
+    FetchDataUserEvent event,
+    Emitter<DataUserBlocState> emit,
+  ) async {
+    emit(DataUserBlocLoading());
+    try {
+      final value = await getIt<DataUserUsecase>().execute();
+      value.fold(
+        (error) {
+          emit(DataUserBlocError(message: error.toString()));
+        },
+        (dataUser) {
+          emit(DataUserBlocLoaded(listUsers: dataUser));
+        },
+      );
+    } catch (e) {
+      emit(DataUserBlocError(message: e.toString()));
+    }
+  }
+}
